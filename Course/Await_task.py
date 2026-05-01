0496c56bd41d99c0d86a036da6f28dbbccb4f922
xghostyyy <140622593+xghostyyy@users.noreply.github.com>
2026-05-01 17:16:04 +0300

    Add files via upload
    В данных файлах будет тяжело проследить какую то последовательность, т.к. сохранял я их абсолютно рандомно

---
+++ Course/Await_task.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from math import sin, log
+import time
+
+link = "https://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    options = Options()
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+    
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    browser.get(link)
+    
+    WebDriverWait(browser, 13).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    browser.find_element(By.ID, 'book').click()
+    x = browser.find_element(By.ID, 'input_value').text
+    res = log(abs(12*sin(int(x))))
+    browser.find_element(By.ID, 'answer').send_keys(str(res))
+    browser.find_element(By.ID, "solve").click()
+    #-----------------------------------
+finally:
+    time.sleep(30)
+    browser.quit()
---
+++ Course/RegForm_task.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    first_name = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+    first_name.send_keys("Ivan")
+
+    last_name = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+    last_name.send_keys("Petrov")
+
+    email = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+    email.send_keys("test@test.com")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+
+finally:
+    time.sleep(10)
+    browser.quit()
